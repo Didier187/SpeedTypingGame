f67ec62782b04ffa806003f4cbe9199a29b2c0f0
Didier <Kdondidier@gmail.com>
2021-10-06 16:44:16 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# React speed typing game
+
+Test your typing speed with this small react project
+## how it works
+- Click start button
+	- The keyborad focus with automatically be in the input field
+	- The time will start counting from deafult time which is 30 secs
+	- At the end of the time you will see how many words you typed in as many seconds
+
+## React feautures covered
+
+1. Hooks
+	- useEffect
+	- useState
+	- useRef
+	- custom hooks
+
+2. Functional components
+
+Quick start:
+
+```
+$ yarn # npm install
+$ yarn start # npm start
+````
+
+ 
+Contact me <kdondidier@gmail.com>
+Happy Coding!
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect, useRef} from "react"
+import useGameLogic from "./useGameLogic"
+
+
+function App() {
+
+const [textBoxRef, handleChange,text,timeRemaining,startGame,isTimeRunning,wordCount] = useGameLogic()    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
+}
+
+export default App
